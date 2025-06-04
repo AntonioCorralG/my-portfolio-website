8b8a925f0a9d03ac89bb9da37eb205c05f57a1fb
Apoorv <Apoorv@Mac.lan>
2025-06-03 20:48:15 -0700

    update4 vit config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://antoniocorralg.github.io/my-portfolio-website/',
+  base: './',
   plugins: [react(),  tsconfigPaths()],
 })
